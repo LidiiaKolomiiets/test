4ecb47e01b3b89198102c1feb1b75339fb338b90
Kolomiiets Lidiia <kolomieclidia7@gmail.com>
2023-09-28 22:02:55 +0300

    add basket icon+number

--- src/headers/Headers.jsx
+++ src/headers/Headers.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 import logo from './logo.png'
-import basket from '../basket/imageBasket/basket-min.png'
+import baskets from '../basket/imageBasket/basket-min.png'
 import NavItem from './NavItem.jsx';
 import './headers.css'
+import { useSelector } from "react-redux";
 
 export default () => {
+  const basket = useSelector(state => state.basket)
+  const basketCount = basket.length
   return <header className="header">
     <img src={logo} alt="Моє зображення" />
     <nav className="nav">
     < NavItem title="Головна" url="/" />
     < NavItem title="Смузі конструктор" url="smoothies" />
     < NavItem title="Про нас" url="about" />
-    <NavItem title={<img src={basket} className="basket-icon"/>} url="basket"/>
+    <NavItem title={<><img src={baskets} className="basket-icon"/>{basketCount > 0 ? `${basketCount}` : ""}</>} url="basket"/>
     </nav>
   </header>
 }
